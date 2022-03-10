63e711c87773df959d07132d584e4375c531b543
miinfant <55447609+miinfant@users.noreply.github.com>
2022-03-10 17:58:16 +0100

    commit ej1

---
+++ main.kt
@@ -0,0 +1,26 @@
+import kotlinx.coroutines.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+val formatter = DateTimeFormatter.ISO_LOCAL_TIME
+val time = { formatter.format(LocalDateTime.now()) }
+
+suspend fun getValue(): Double {
+    println("entering getValue() at ${time()}")
+    delay(3000)
+    println("leaving getValue() at ${time()}")
+    return Math.random()
+}
+
+fun main() {
+   val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+   repeat(3) {
+       GlobalScope.launch {
+           println("${Thread.currentThread()} has started")
+           for (i in states) {
+               println("${Thread.currentThread()} - $i")
+               delay(5000)
+           }
+       }
+   }
+}
